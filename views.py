e900e2ce03cff8227da261ef0630759219442a2c
zgreenholt <zgrnhlt@udel.edu>
2019-10-30 20:09:40 -0400

    Added documentation for functions and potentially confusing bits

--- views.py
+++ views.py
@@ -21,9 +21,6 @@ def open_door(world: dict, where: str) -> str:
     :param world: The current world
     :param where: The new location to move to
     :return: The HTML to show the player
-
-    world['location'] = where
-    return GAME_HEADER+ENCOUNTER_MONSTER.format(where)
     """
 
     if where == "Earth":
@@ -67,7 +64,8 @@ def open_door(world: dict, where: str) -> str:
 def new_conflict(world: dict, where: str) -> str:
     """
     resolve one of the gods' conflicts from the throne room. Travel to either Ares
-    and Athena, Aphrodite and Hera, or Persephone and Hades
+    and Athena, Aphrodite and Hera, or Persephone and Hades.
+
     :param world: the current world
     :param where: the new place to travel to
     :return: the HTML to show the player
@@ -81,16 +79,33 @@ def new_conflict(world: dict, where: str) -> str:
 
 @simple_route("/goto/apples/<num>")
 def increment_apples(world: dict, num: str) -> str:
+    """
+    Navigates through the Aphrodite and Hera mini-game based
+    on the number of apples that have been collected.
+
+    :param world: the current world
+    :param num: the current number of apples collected
+    :return: the HTML to show the player
+    """
     if int(num) == 4:
         world["tasks"][1] = True
+        # signifies completing the Aphrodite and Hera mini-game
     world["apples"] = int(num)
     return render_template("apples.html", num_of_apples=world["apples"], tasks=world["tasks"], inventory=world["inventory"],
                                enlarge=world['enlarge'], header="Collect the Golden Apples")
 
 @simple_route("/goto/seeds/goto/<num>")
 def increment_seeds(world: dict, num: str) -> str:
+    """
+    Navigates through the Persephone and Hades mini-game based
+    on the number of seeds that have been collected.
+    :param world: the current world
+    :param num: the current number of seeds collected
+    :return: the HTML to show the player
+    """
     if int(num) == 4:
         world["tasks"][2] = True
+        # signifies completing the Persephone and Hades mini-game
     if int(num) == 3:
         return render_template("seeds_3.html")
     else:
@@ -101,11 +116,28 @@ def increment_seeds(world: dict, num: str) -> str:
 
 @simple_route("/goto/seeds/goto/2/<num>")
 def new_quiz(world: dict, num: str) -> str:
+    """
+    Navigates through the quiz section of the Persephone and Hades mini-game based
+    on the number of quizzes that have been completed.
+
+    :param world: the current world
+    :param num: the current number of quizzes completed
+    :return: the HTML to show the player
+    """
     world["quiz"] = int(num)
     return render_template("seeds.html", num_of_seeds=world["seeds"], tasks=world["tasks"], quiz=world["quiz"], header="Collect the Seeds")
 
 
 def play_war(x: int, y: int, z: int, tasks: [bool]) -> str:
+    """
+    Compares card values in the war mini-game.
+
+    :param x: Player's card value
+    :param y: Athena's card value
+    :param z: Ares' card value
+    :param tasks: Currently completed mini-games
+    :return: HTML to show the player
+    """
     a = random.randrange(2, 15)
     b = random.randrange(2, 15)
     c = random.randrange(2, 15)
@@ -125,6 +157,7 @@ def play_war(x: int, y: int, z: int, tasks: [bool]) -> str:
     elif y == z:
         if x > y:
             tasks[0] = True
+            # signifies completing the Ares and Athena mini-game
             return display + check_tasks(tasks)
         else:
             return display + render_template("/War_Outcomes/Loss.html")
@@ -138,6 +171,13 @@ def play_war(x: int, y: int, z: int, tasks: [bool]) -> str:
 
 
 def check_tasks(tasks: [bool]) -> str:
+    """
+    Checks how many mini-games are currently completed
+    to determine the links that will appear after completing the war mini-game.
+
+    :param tasks: The currently completed mini-games
+    :return: HTML to show the player
+    """
     if (not tasks[1]) and (not tasks[2]):
         return render_template("/War_Outcomes/win_options/war_first.html")
     if tasks[1] and (not tasks[2]):
